a27305e27128340ec2ac455ba8019bc6e2a37a1b
omargonza <gonzamartinez1081@gmail.com>
2022-11-26 05:59:41 -0300

    CartWitch agregado

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import ItemListContainer from './componentes/ItemListContainer/ItemListContainer';
 import NavBar from './componentes/NavBar';
-
+import  'boxicons';
+import CartWitch from './componentes/CartWitch/CartWitch';
 function App() {
   return (
    <>
 
 <NavBar/>
+<CartWitch/> 
 <ItemListContainer onClick={console.log}>Aguante</ItemListContainer>
 <ItemListContainer onClick={console.log}>la</ItemListContainer>
 <ItemListContainer onClick={console.log}>Selección!!</ItemListContainer>
---
+++ src/componentes/CartWitch/CartWitch.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+export default function CartWitch() {
+    return (
+        <div className='carrito'>
+            <box-icon name="BsCart4 " ></box-icon>
+            <span className='item_total'>0</span> </div>
+    )
+}
